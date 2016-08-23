8075e055bd2812201463123a870939c7680c09d8
George Witteman <georgewitteman@gmail.com>
2016-08-23 13:34:07 -0400

    Add SASS

--- header.php
+++ header.php
@@ -4,19 +4,13 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
   <header>
     <h1><a href="<?php echo esc_url( home_url( '/' ) );?>"><?php bloginfo("name"); ?></a></h1>
-    <p class="tagline">
-      <?php
-      echo get_bloginfo('description');
-      ?>
-    </p>
+    <p class="tagline"><?php echo get_bloginfo('description'); ?></p>
   </header>
 
   <?php wp_nav_menu( array(
--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Template part for when there is no content
+ */
+
 if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
   <p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', '_s' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
