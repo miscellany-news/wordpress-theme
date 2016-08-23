a0e5090abda72005736712e8c47ad105522a4a50
George Witteman <georgewitteman@gmail.com>
2016-08-23 15:17:12 -0400

    Show recommended plugins

--- functions.php
+++ functions.php
@@ -2,5 +2,7 @@
 
 require_once('functions/setup.php');
 require_once('functions/remove-bloat.php');
+require_once('functions/class-tgm-plugin-activation.php');
+require_once('functions/recommend-plugins.php');
 
 ?>
