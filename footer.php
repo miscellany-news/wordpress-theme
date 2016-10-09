44be9559aa8a07bc9f1440b19740b7417f901b9e
George Witteman <georgewitteman@gmail.com>
2016-10-09 15:12:05 -0400

    Move aux menu and search bar to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
-  <footer>&copy; <?php bloginfo("name"); echo " " . date('Y'); ?></footer>
+  <footer>
+		<div class="aux-menu">
+		  <?php wp_nav_menu( array(
+		    'theme_location' => 'auxiliary-menu',
+		    'container' => 'nav',
+				'menu_id' => 'aux-nav-1',
+		    'container_class' => 'aux-navigation'
+		  )); ?>
+			<?php get_search_form() ?>
+		</div>
+		&copy; <?php bloginfo("name"); echo " " . date('Y'); ?>
+	</footer>
   <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -9,16 +9,8 @@
 <body <?php body_class(); ?>>
 
   <header class="header-main">
-		<div class="aux-header">
-		  <?php wp_nav_menu( array(
-		    'theme_location' => 'auxiliary-menu',
-		    'container' => 'nav',
-				'menu_id' => 'aux-nav-1',
-		    'container_class' => 'aux-navigation'
-		  )); ?>
-			<?php get_search_form() ?>
-		</div>
 		<a href="#main-nav-1" class="screen-reader-text">Skip to navigation</a>
+		<a href="#aux-nav-1">Menu</a>
     <h1><a href="<?php echo esc_url( home_url( '/' ) );?>"><?php bloginfo("name"); ?></a></h1>
     <p class="tagline"><?php echo get_bloginfo('description'); ?></p>
   </header>
