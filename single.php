64ce8636df62bed45db2a2b83797bfce4aa6683e
George Witteman <georgewitteman@gmail.com>
2016-11-29 18:04:23 -0500

    Single posts

--- single.php
+++ single.php
@@ -1,19 +1,19 @@
 <?php get_header();  ?>
 
-<main class="single">
+<main class="single-content">
   <?php 
   // Start the loop.
   while ( have_posts() ) : the_post(); ?>
-
-    <article id="post-<?php the_ID(); ?>" <?php post_class('single-post'); ?>>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('post-single'); ?>>
   <header class="post-header">
 <div class="share-links">
-        <div class="share-box">
-        <a href="http://www.facebook.com/sharer/sharer.php?u=<?php print(urlencode(get_permalink())); ?> &title=<?php print(urlencode(the_title())); print(urlencode(" - The Miscellany News")); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/share-fb.svg" class="share-image"></a>
-        <a href="http://twitter.com/intent/tweet?status=<?php print(urlencode(the_title())); print(urlencode(" - The Miscellany News")); ?>+<?php print(urlencode(get_permalink())); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/share-tw.svg" class="share-image"></a>
-        <a href="mailto:?subject=<?php print(urlencode(the_title())); print(urlencode(" - The Miscellany News")); ?>&body=Check out this article from The Miscellany News <?php print(urlencode(the_permalink())); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/share-em.svg" class="share-image"></a>
-      </div>
-      </div>
+<div class="share-box">
+<a href="http://www.facebook.com/sharer/sharer.php?u=<?php print(urlencode(get_permalink())); ?> &title=<?php print(urlencode(the_title())); print(urlencode(" - The Miscellany News")); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/share-fb.svg" class="share-image"></a>
+<a href="http://twitter.com/intent/tweet?status=<?php print(urlencode(the_title())); print(urlencode(" - The Miscellany News")); ?>+<?php print(urlencode(get_permalink())); ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/share-tw.svg" class="share-image"></a>
+<a href="mailto:?subject=<?php print(urlencode(the_title())); print(urlencode(" - The Miscellany News")); ?>&body=Check out this article from The Miscellany News <?php print(urlencode(the_permalink())); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/share-em.svg" class="share-image"></a>
+</div>
+</div>
+
     <?php foreach(get_the_category() as $category) : ?>
     <div class="post-category"><a href="<?php echo get_category_link($category->term_id); ?>"><?php echo $category->name; ?></a></div>
     <?php endforeach; ?>
@@ -35,32 +35,25 @@
      
   </header>
   <div class="post-left">
-  <?php
-  get_template_part( 'template-parts/featured-image', get_post_format() );
-  ?>
-  
+    <?php
+    get_template_part( 'template-parts/featured-image', get_post_format() );
+    ?>
+    <div class="post-content">
+    <?php
+    the_content();
 
-  
-    
-    
-      
+    wp_link_pages(array( 'before' => '<nav class="link-pages">', 'after'  => '</nav>'));
+    ?>
 
-  <div class="post-content">
-  <?php
-  the_content();
-
-  wp_link_pages(array( 'before' => '<nav class="link-pages">', 'after'  => '</nav>'));
-  ?>
-
+    </div>
   </div>
   
-  <div class="single-sidebar">
+  <div class="post-right">
     <?php if ( is_active_sidebar( 'post-sidebar' ) ) : ?>
       <?php dynamic_sidebar( 'post-sidebar' ); ?>
     <?php endif; ?>
   </div>
   
-  </div>
 
 </article>
 <?php
