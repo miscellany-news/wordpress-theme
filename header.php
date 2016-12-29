8596b3c4745b9a2b79b91ee2c18349cf481e6975
George Witteman <georgewitteman@gmail.com>
2016-12-29 18:35:23 -0500

    Sections scroller

--- header.php
+++ header.php
@@ -10,32 +10,50 @@
 <div id="hamburger-page-background" class="hamburger-background"></div>
   <!-- Site header -->
   <header class="site-header">
+    <div class="header-container">
 
-    <!-- Hamburger icon -->
-    <figure id="hamburger-main" class="hamburger-icon">
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
-    </figure>
+      <!-- Hamburger icon -->
+      <figure id="hamburger-main" class="hamburger-icon">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </figure>
 
-    <!-- Search icon -->
-    <a href="#">
-      <img src="<?php echo get_template_directory_uri() . '/img/search.svg'?>" class="search-icon" id="search-main">
-    </a>
+      <!-- Search icon -->
+      <a href="#">
+        <img src="<?php echo get_template_directory_uri() . '/img/search.svg'?>" class="search-icon" id="search-main">
+      </a>
 
-    <!-- The Miscellany News logo -->
-    <a class="site-title" href="<?php echo esc_url( home_url( '/' ) );?>">
-      <img src="<?php echo get_template_directory_uri() . '/img/logo.svg'?>" class="site-logo">
-    </a>
+      <!-- The Miscellany News logo -->
+      <a class="site-title" href="<?php echo esc_url( home_url( '/' ) );?>">
+        <img src="<?php echo get_template_directory_uri() . '/img/logo.svg'?>" class="site-logo">
+      </a>
 
-    <!-- Subtitle -->
-    <span class="site-subtitle">
-      <?php
-      date_default_timezone_set('America/New_York');
-      $day = date('l',time());
-      $date = date('F j, Y', time());
-      ?>
-      Since 1866 | <em><?php echo $day; ?>,</em> <?php echo $date; ?>
-    </span>
+      <!-- Subtitle -->
+      <!--<span class="site-subtitle">
+        <?php
+        date_default_timezone_set('America/New_York');
+        $day = date('l',time());
+        $date = date('F j, Y', time());
+        ?>
+        Since 1866 | <em><?php echo $day; ?>,</em> <?php echo $date; ?>
+      </span> -->
+
+    </div>
 
   </header>
+  <nav class="hn">
+    <div class="hn-container">
+      <span class="hn-home">Home</span>
+      <?php
+      wp_nav_menu( array(
+        'theme_location' => 'site-sections',
+        'container' => '',
+        'menu_class' => 'hn-menu',
+        'link_before' => '<span>',
+        'link_after' => '</span>',
+        'menu_id' => 'hn-menu'
+      ));
+      ?>
+    </div>
+  </nav>
