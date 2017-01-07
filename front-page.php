9dc5e33cf75bad04068665de74499c34a5a73b75
George Witteman <georgewitteman@gmail.com>
2017-01-07 13:20:42 -0500

    Section rulers

--- front-page.php
+++ front-page.php
@@ -19,6 +19,87 @@
     endwhile; wp_reset_postdata();
     ?>
   </section>
+<hr class="section-rule">
+  <section class="front-section section-news">
+    <h2 class="section-title">News</h2>
+    <div class="grid">
+      <?php
+      $args = array('posts_per_page' => 3, 'offset' => 0, 'category_name' => 'news');
+      $loop = new WP_Query( $args );
+      while ($loop->have_posts()) : $loop->the_post();
+        get_template_part('template-parts/front');
+      endwhile; wp_reset_postdata();
+      ?>
+    </div>
+  </section>
+
+  <hr class="section-rule">
+
+  <section class="front-section section-features">
+    <h2 class="section-title">Features</h2>
+    <div class="grid">
+      <?php
+      $args = array('posts_per_page' => 6, 'offset' => 0, 'category_name' => 'features');
+      $loop = new WP_Query( $args );
+      while ($loop->have_posts()) : $loop->the_post();
+        get_template_part('template-parts/front');
+      endwhile; wp_reset_postdata();
+      ?>
+    </div>
+  </section>
+<hr class="section-rule">
+  <section class="front-section section-arts">
+    <h2 class="section-title">Arts</h2>
+    <div class="grid">
+      <?php
+      $args = array('posts_per_page' => 3, 'offset' => 0, 'category_name' => 'arts');
+      $loop = new WP_Query( $args );
+      while ($loop->have_posts()) : $loop->the_post();
+        get_template_part('template-parts/front');
+      endwhile; wp_reset_postdata();
+      ?>
+    </div>
+  </section>
+
+<hr class="section-rule">
+  <section class="front-section section-sports">
+    <h2 class="section-title">Sports</h2>
+    <div class="grid">
+      <?php
+      $args = array('posts_per_page' => 3, 'offset' => 0, 'category_name' => 'sports');
+      $loop = new WP_Query( $args );
+      while ($loop->have_posts()) : $loop->the_post();
+        get_template_part('template-parts/front');
+      endwhile; wp_reset_postdata();
+      ?>
+    </div>
+  </section>
+<hr class="section-rule">
+  <section class="front-section section-opinions">
+    <h2 class="section-title">Opinions</h2>
+    <div class="grid">
+      <?php
+      $args = array('posts_per_page' => 3, 'offset' => 0, 'category_name' => 'opinions');
+      $loop = new WP_Query( $args );
+      while ($loop->have_posts()) : $loop->the_post();
+        get_template_part('template-parts/front');
+      endwhile; wp_reset_postdata();
+      ?>
+    </div>
+  </section>
+  <hr class="section-rule">
+  <section class="front-section section-humor">
+    <h2 class="section-title">Humor & Satire</h2>
+    <div class="grid">
+      <?php
+      $args = array('posts_per_page' => 3, 'offset' => 0, 'category_name' => 'humor');
+      $loop = new WP_Query( $args );
+      while ($loop->have_posts()) : $loop->the_post();
+        get_template_part('template-parts/front');
+      endwhile; wp_reset_postdata();
+      ?>
+    </div>
+  </section>
 
 </main>
 
--- template-parts/front.php
+++ template-parts/front.php
@@ -1,18 +1,18 @@
-<article class="front-article">
+<article class="front-article fn-article">
 
-  <a class="front-image" href="<?php the_permalink(); ?>">
+  <a class="front-image fn-image" href="<?php the_permalink(); ?>">
     <?php the_post_thumbnail(array(1440,860))?>
   </a>
 
-  <div class="front-content">
+  <div class="front-content fn-content">
 
-    <h3 class="front-title">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="front-title-link">
+    <h3 class="front-title fn-title">
+      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="front-title-link fn-title-link">
         <?php the_title();?>
       </a>
     </h3>
 
-    <p class="front-author">By <?php miscellanynews_get_author_link(); ?></p>
+    <p class="front-author fn-author">By <?php miscellanynews_get_author_link(); ?></p>
 
   </div>
 
