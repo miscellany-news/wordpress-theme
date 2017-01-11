a372a979dccd7c83338c76278111a24a2c9e5a2a
George Witteman <georgewitteman@gmail.com>
2017-01-11 12:07:17 -0500

    Starter background header

--- header.php
+++ header.php
@@ -10,22 +10,30 @@
 <div id="hamburger-page-background" class="hamburger-background"></div>
   <!-- Site header -->
   <header class="site-header">
+
+    <div class="toolbar">
+      <div class="toolbar-container">
+        Toolbar
+      </div>
+    </div>
+
     <div class="header-container">
-
       <!-- The Miscellany News logo -->
-      <a class="site-title" href="<?php echo esc_url( home_url( '/' ) );?>">
-        <img src="<?php echo get_template_directory_uri() . '/img/banner2x.png'?>" class="site-logo">
-      </a>
-
+      <h1 class="site-name">
+        <a class="site-title" href="<?php echo esc_url( home_url( '/' ) );?>">The Miscellany News</a>
+      </h1>
+      <p class="site-subheading">
+        Since 1866 | <b>Wednesday,</b> January 11, 2017
+      </p>
     </div>
 
     <nav class="hn">
       <div class="hn-container">
-        <div class="hn-search-overlay" id="hn-search-overlay"></div>
+        <!--<div class="hn-search-overlay" id="hn-search-overlay"></div>
         <div class="hn-search-container">
           <a href="#" class="hn-search" id="hn-search"></a>
           <?php get_search_form(); ?>
-        </div>
+        </div>-->
         <?php
         wp_nav_menu( array(
           'theme_location' => 'site-sections',
