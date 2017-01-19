36dd9fa4f819a1f061afd7912d5accd7376e375d
George Witteman <georgewitteman@gmail.com>
2017-01-19 16:54:50 -0500

    Update version number

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function() {
 	return gulp.src('./sass/style.scss')
 		.pipe(sass().on('error', sass.logError))
-	  .pipe(gulp.dest('./'));
-});
-
-gulp.task('deploy', function() {
-	return gulp.src('./sass/style.scss')
-		.pipe(sass().on('error', sass.logError))
-	  .pipe(cleanCSS({ 'keepSpecialComments': 1 }))
+    .pipe(cleanCSS( { processImport: false }))
 	  .pipe(gulp.dest('./'));
 });
 
