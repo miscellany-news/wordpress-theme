520bc1bd0ce011d7be73ba581017138661503a0e
Mohamad Safadieh <me@msafadieh.com>
2019-10-07 01:22:38 -0400

    remove medium front page articles

--- front-page.php
+++ front-page.php
@@ -11,14 +11,6 @@
       get_template_part('template-parts/front', 'large');
     endwhile; wp_reset_postdata();
     ?>
-    <?php
-    $args = array('posts_per_page' => 4, 'offset' => 1, 'tag' => 'slider');
-    $loop = new WP_Query( $args );
-
-    while ($loop->have_posts()) : $loop->the_post();
-      get_template_part('template-parts/front', 'medium');
-    endwhile; wp_reset_postdata();
-    ?>
   </section>
 
   <?php if ( is_active_sidebar( 'front-ad-area' ) ) : ?>
