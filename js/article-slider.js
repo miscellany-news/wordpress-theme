61370a5102ce5a48dea38a5a40f2c88f31f42413
Mohamad Safadieh <me@msafadieh.com>
2019-11-07 00:16:38 -0500

    refactor slider script

--- js/article-slider.js
+++ js/article-slider.js
@@ -1,40 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
   const featuredContainer = document.getElementsByClassName("section-featured")[0];
-  const dotContainer = document.createElement("div");
-  const frontArticles = document.getElementsByClassName("fl-article");
-  const sliderDots = [];
-  const articleCount = frontArticles.length;
-
   if (!featuredContainer) return;
-  featuredContainer.onmouseover = () => mouseIn = true;
-  featuredContainer.onmouseleave = () => mouseIn = false;
 
+  const frontArticles = document.getElementsByClassName("fl-article");
+  const dotContainer = document.createElement("div");
   dotContainer.classList.add("slider-dot-container");
 
   let mouseIn = false;
   let currentArticle = 0;
 
-  function switchTo(n) {
-    const nextArticle = n % articleCount;
-    frontArticles[currentArticle].classList.toggle("fl-article-visible");
-    sliderDots[currentArticle].classList.toggle("slider-dot-active")
-    frontArticles[nextArticle].classList.toggle("fl-article-visible");
-    sliderDots[nextArticle].classList.toggle("slider-dot-active")
-    currentArticle = nextArticle;
-  }
+  featuredContainer.onmouseover = () => mouseIn = true;
+  featuredContainer.onmouseleave = () => mouseIn = false;
 
-  for (let i = 0; i < articleCount; i++) {
+  for (let i = 0; i < frontArticles.length; i++) {
     let dot = document.createElement("div");
     dot.classList.add("slider-dot");
     dot.onclick = () => switchTo(i);
     dotContainer.appendChild(dot);
-    sliderDots.push(dot);
   }
-
-  sliderDots[0].classList.toggle("slider-dot-active")
+  toggle(sliderDots[0], "slider-dot-active");
   featuredContainer.appendChild(dotContainer);
 
   setInterval(() => {
     if (!mouseIn) switchTo(currentArticle+1);
   }, 5000);
+
+  function toggle(el, className) {
+    return el.classList.toggle(className);
+  }
+
+  function switchTo(n) {
+    const nextArticle = n % articleCount;
+    toggle(frontArticles[currentArticle], "fl-article-visible");
+    toggle(dotContainer.children[currentArticle], "slider-dot-active");
+    toggle(frontArticles[nextArticle], "fl-article-visible");
+    toggle(sliderDots.children[nextArticle], "slider-dot-active")
+
+    currentArticle = nextArticle;
+  }
+
 });
